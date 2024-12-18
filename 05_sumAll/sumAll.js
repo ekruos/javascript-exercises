247ec40e3962f1bd500939a6a83747f338d65e1c
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2024-12-18 23:56:38 +0200

    Complete js ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (typeof first !== "number" || typeof second !== "number") {
+        return "ERROR";
+    }
+    if (Math.floor(first) !== first || Math.floor(second) !== second) {
+        return "ERROR";
+    }
+    if (first < 0 || second < 0) {
+        return "ERROR";
+    }
+
+    if (first <= second) {
+        small = first;
+        big = second;
+    } else {
+        small = second;
+        big = first;
+    }
+
+    let sum = 0;
+    for (let i = small; i <= big; i++) {
+        sum += i;
+    }
+    return sum;
+    
+
 
 };
 
