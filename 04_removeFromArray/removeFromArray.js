3b0aa83e57bf2887578495bc00ea4bf174b211d2
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2024-12-18 23:00:37 +0200

    Complete js ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(list, ...items) {
+    finalList = [];
+    for (i = 0; i < list.length; i++) {
+        if (!(items.includes(list[i]))) {
+            finalList.push(list[i]);
+        }
+    }
+    return finalList;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
