ad00329897483cf2ab6237b97a63bd065ef868b7
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2024-12-19 00:38:23 +0200

    Complete js ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  result = (degree - 32) * 5/9;
+  return +result.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  result = 9/5 * degree + 32;
+  return +result.toFixed(1);
 };
 
 // Do not edit below this line
