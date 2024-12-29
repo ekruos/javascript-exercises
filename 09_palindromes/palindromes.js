50cd2d3ccd8bb331cb3c02bd4bfef037c143e6a9
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2024-12-29 14:48:18 +0200

    Complete ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    if (str[str.length-1] === "." || str[str.length-1] === "!") {
+        str = str.substring(0,str.length-1);
+    }
+    let newStr = "";
+    for (i = 0; i < str.length; i++) {
+        if (str[i] !== " " && str[i] !== ",") {
+            newStr += str[i];
+        }
+    }
+    let reverse = "";
+    for (i = newStr.length-1; i >= 0; i--) {
+        reverse += newStr[i];
+    }
+    return reverse == newStr;
 
 };
 
