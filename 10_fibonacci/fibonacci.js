1f32a1536f8e50cba4b53d377bc745742276b0e1
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2024-12-29 15:01:56 +0200

    Complete ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (typeof number == "string") {
+        number = +number;
+    }
+    if (number < 0) {
+        return "OOPS";
+    }
+    let fibo = [];
+    fibo[0] = 0;
+    fibo[1] = 1;
+    fibo[2] = 1;
+    for (i = 3; i <= number; i++) {
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    return fibo[number];
 
 };
 
