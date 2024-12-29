732ac27992d49ed09fb570775bc2e4219d83af42
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2024-12-29 14:27:21 +0200

    Complete ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(...args) {
+	let sum = 0;
+  for (let arg of args) {
+    sum += arg;
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr.length === 0) {
+    return 0;
+  } else {
+    result = 0;
+    for (const item of arr) {
+      result += item;
+    }
+    return result;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let product = 1;
+  for (let item of args) {
+    product *= item;
+  } 
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a  ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+    return 1;
+  } else {
+    result = 1;
+    for (i = 1; i <= number; i++) {
+      result *= i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
