8956eeaa044d077ef8627752961b9b24bf4d8886
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2024-12-29 15:35:18 +0200

    Complete ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    for (const item of arr) {
+        if (!("yearOfDeath" in item)) {
+            item.yearOfDeath = 2024;
+        }
+    }
+    let oldest = arr[0];
+    let oldestAge = arr[0].yearOfDeath - arr[0].yearOfBirth;
 
+    for (const person of arr) {
+        if (person.yearOfDeath - person.yearOfBirth > oldestAge) {
+            oldest = person;
+            oldestAge = person.yearOfDeath - person.yearOfBirth;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
